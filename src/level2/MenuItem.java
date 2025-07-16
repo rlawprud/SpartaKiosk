098b4e4e8a44d5ba3e8146629b6766f0caf5c5ab
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-07-16 20:42:46 +0900

    level 2 해결 및 level 3 기본 클래스 추가

--- src/level2/MenuItem.java
+++ src/level2/MenuItem.java
@@ -1,26 +1,17 @@
 package level2;
 
 public class MenuItem {
-    private String name;
-    private int price;
-    private String info;
+    // 메뉴명, 가격, 정보를 저장할 변수 캐스팅
+    String name;
+    int price;
+    String info;
 
-    void setName (String name){
-        this.name = name;
-    }
-    void setPrice (int price) {
-        this.price = price;
-    }
-    void setInfo (String info) {
-        this.info = info;
-    }
-    void setMenu (String name, int price, String info){
+    // 생성자
+    public MenuItem(String name, int price, String info){
+        // 이 클래스로 만들어진 객체의 메뉴명과 가격, 정보를 매개로 받아 저장합니다.
+        // 사용할 경우 -> new 키워드를 사용하여 새 인스턴스를 만들 때 매개변수를 주어야 합니다.
         this.name = name;
         this.price = price;
         this.info = info;
     }
-
-    String getName() {return name;}
-    int getPrice() {return price;}
-    String getInfo() {return info;}
 }
---
+++ src/level3/Main.java
@@ -0,0 +1,7 @@
+package level3;
+
+public class Main {
+    public static void main (String[] args){
+        // 메인입니다.
+    }
+}
